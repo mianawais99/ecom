0522610129f1a9cae262346cee3695f0436c2ca7
MIAN AWAIS <mianawais4832@hotmail.com>
2022-03-15 02:20:52 +0500

    anchor tag

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/mianawais99/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Developer Link
         </a>
       </header>
     </div>
